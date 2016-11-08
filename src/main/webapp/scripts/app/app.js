ac71e8e046b3d6448b1236d54d6f3db956eb4d21
Kevin Stanco <Kevin@Kevins-Air.fios-router.home>
2016-11-07 22:53:14 -0500

    got angular view working

--- src/main/webapp/scripts/app/app.js
+++ src/main/webapp/scripts/app/app.js
@@ -1,16 +1,25 @@
 // declare a module
-var myAppModule = angular.module('myApp', ['ui-router']);
-myAppModule.config(function(){
+var myAppModule = angular.module('myApp', ['ui.router']);
+myAppModule.config(['$urlRouterProvider','$stateProvider',function($urlRouterProvider, $stateProvider){
 	var home={
 		name: 'home',
 		url: '/home',
 		templateUrl: 'pages/homePanel.html'
 	}
-	var otherwise={
-			name: 'otherwise',
-			url: '/',
-			templateUrl: 'pages/homePanel.html'
+	var messages={
+			name: 'messages',
+			url: '/messages',
+			templateUrl: 'pages/msgPanel.html'
 		}
-//	$stateProvider.state(home);
-//	$stateProvider.state(otherwise);
-});
+	var myAccount={
+			name: 'myAccount',
+			url: '/account',
+			templateUrl: 'pages/accountPanel.html'
+		}
+
+	$stateProvider.state(home);
+	$stateProvider.state(messages);	
+	$stateProvider.state(myAccount);
+	
+	$urlRouterProvider.otherwise('/home');
+}]);
