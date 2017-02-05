f17e04eb8687b7fc15855f8a13cf3000c3b7a405
Kstanco <carcrazy99999@yahoo.com>
2017-02-05 16:49:17 -0500

    and more changes

--- src/main/webapp/scripts/controller/galleryController.js
+++ src/main/webapp/scripts/controller/galleryController.js
@@ -1,6 +1,7 @@
 // declare a controller
 angular.module('myApp').controller("galleryController", ['galleryService', '$scope' ,'photos', function(galleryService, $scope, photos){
 	var self=this;
+	var LOADING = 1;
 	
 	$scope.photos=photos
 	
@@ -13,4 +14,30 @@ angular.module('myApp').controller("galleryController", ['galleryService', '$sco
 		self.selectedPhoto = selectedPhoto;
 	}
 
+	self.fileChanged = function (files) {
+		var i = 0;
+		self.files = files.files;	  
+	    var localFileBlobs = [];
+		r = new FileReader();
+	   
+	    r.onloadend = function(e){
+	    	i++;
+	    	localFileBlobs.push(e.target.result);
+	    	if (i < self.files.length) {
+	    		r.readAsDataURL(self.files[i]);
+	    	}else{
+	    		$scope.$apply(function(){
+	    			self.fileBlobs = localFileBlobs;
+	    			
+	    		});
+	    	}
+	    }
+	 	r.readAsDataURL(self.files[i]);
+	   
+	 }
+	
+	
+	self.submitPhoto= function() {
+		galleryService.postPhotos(self.fileBlob);
+	}
 }]);
