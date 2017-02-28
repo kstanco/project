f998b9eb36d487106909587bfb8bb30933998c89
Kevin Stanco <carcrazy99999@yahoo.com>
2017-02-28 08:47:13 -0500

    added more postrgres features

--- src/main/java/com/charlie/controllers/GalleryController.java
+++ src/main/java/com/charlie/controllers/GalleryController.java
@@ -1,10 +1,11 @@
 package com.charlie.controllers;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -12,47 +13,12 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.charlie.dao.GalleryDAO;
 import com.charlie.entity.PhotoEntity;
-import com.charlie.services.GalleryService;
 
 @RestController
 public class GalleryController {
 
 	@Autowired
 	private GalleryDAO galleryDAO;
-	
-	@RequestMapping(value = "/getphotos", method = RequestMethod.GET)
-	public List<String> getPhotos(){
-		
-		String[] myList = {
-		    "resources/photos/1.JPG", 
-			"resources/photos/2.JPG", 
-			"resources/photos/3.JPG", 
-			"resources/photos/4.JPG", 
-			"resources/photos/5.JPG", 
-			"resources/photos/6.JPG", 
-			"resources/photos/7.JPG", 
-			"resources/photos/IMG_0001.JPG", 
-			"resources/photos/IMG_0002.JPG", 
-			"resources/photos/IMG_0004.JPG", 
-			"resources/photos/IMG_0005.JPG", 
-			"resources/photos/IMG_0006.JPG", 
-			"resources/photos/IMG_0007.JPG", 
-			"resources/photos/IMG_0008.JPG", 
-			"resources/photos/IMG_0009.JPG", 
-			"resources/photos/IMG_0016.JPG", 
-			"resources/photos/IMG_0019.JPG", 
-			"resources/photos/IMG_0020.JPG", 
-			"resources/photos/IMG_0041.JPG", 
-			"resources/photos/IMG_0059.JPG", 
-			"resources/photos/IMG_0064.JPG", 
-			"resources/photos/IMG_0067.JPG", 
-			"resources/photos/whatsUp.ico"
-		};
-		
-		List<String> photoList = Arrays.asList(myList);
-		
-		return photoList;
-	}
 	
 	@RequestMapping(value = "/postphotos", method = RequestMethod.POST)
 	public void postPhotos(@RequestBody List<String>photoList){
@@ -70,10 +36,14 @@ public class GalleryController {
 	}
 	
 	@RequestMapping(value = "/findphotos", method = RequestMethod.GET)
-	public List<PhotoEntity> findPhotos() {
+	public Page<PhotoEntity> findPhotos() {
 		
-		return galleryDAO.findAll();
+		return galleryDAO.findAllByOrderByTimestampDesc(new PageRequest(0,4));
 	}
 
-	
+	@RequestMapping(value = "/deleteallphotos", method = RequestMethod.GET)
+	public void deletePhotos() {
+		galleryDAO.deleteAll();
+		
+	}
 }
--- src/main/java/com/charlie/entity/PhotoEntity.java
+++ src/main/java/com/charlie/entity/PhotoEntity.java
@@ -1,43 +1,63 @@
 package com.charlie.entity;
 
+
+
+import java.util.Date;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 
 @Entity
-@Table(name="gallery")
+@Table(name = "gallery")
 public class PhotoEntity {
-  @Id 
-  @Column(name="id")
-  Integer photoId;
+	@Id
+	@SequenceGenerator(name = "gallery_id")
+	@Column(name = "id")
+	Integer photoId;
 
-  @Column(name="photoblob")
-  String photoBlob;
-  
-  public PhotoEntity() {
-	  super();
-  };
-  
-  public PhotoEntity(Integer id) {
-	  this();
-	  this.photoId=id;
-  }
+	@Column(name = "photoblob")
+	String photoBlob;
 
-public int getPhotoId() {
-	return photoId;
-}
+	@Column(name = "date_added", columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date timestamp;
 
-public void setPhotoId(Integer photoId) {
-	this.photoId = photoId;
-}
+	public PhotoEntity() {
+		super();
+	};
 
-public String getPhotoBlob() {
-	return photoBlob;
-}
+	public PhotoEntity(Integer id) {
+		this();
+		this.photoId = id;
+	}
 
-public void setPhotoBlob(String photoBlob) {
-	this.photoBlob = photoBlob;
-};
-  
+	public Date getTimestamp() {
+		return timestamp;
+	}
+
+	public void setTimestamp(Date timestamp) {
+		this.timestamp = timestamp;
+	}
+
+	public int getPhotoId() {
+		return photoId;
+	}
+
+	public void setPhotoId(Integer photoId) {
+		this.photoId = photoId;
+	}
+
+	public String getPhotoBlob() {
+		return photoBlob;
+	}
+
+	public void setPhotoBlob(String photoBlob) {
+		this.photoBlob = photoBlob;
+	};
+
 }
